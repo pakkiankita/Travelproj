5d9ab7dc16dbc9b9ed71be3cd1f4c8d5fedc9f14
pakkiankita <ankitapakki@gmail.com>
2024-07-08 23:28:10 +0530

    second  commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -45,7 +45,36 @@ function App() {
             </svg>
           </button>
         </div>
-        <div className="flex gap-2  border border-gray-300 rounded-full py-2 px-4 "></div>
+        <div className="flex gap-2  border border-gray-300 rounded-full py-2 px-4 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+          <div className="bg-gray-500 text-white rounded full border border-gray-500 overflow-hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-6 relative top-1"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+        </div>
       </header>
     </div>
   );
