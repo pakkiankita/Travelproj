57c7ae6855e0b9bb6635d79a695848c8a3d031c9
unknown <arpitapakki@gmail.com>
2024-07-09 18:34:18 +0530

    third

---
+++ client/src/pages/PlacesPage.jsx
@@ -0,0 +1,12 @@
+
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+export default function PlacesPage() {
+    return (
+        <div>
+            my place
+        </div>
+    );
+}
+
