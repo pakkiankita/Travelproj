bb1331904f9eefae24704566e88d6cff0dd35e81
Barsha1-3 <barshasahoo0511@gmail.com>
2024-07-10 23:57:13 +0530

    fourth

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -44,10 +44,24 @@ export default function PlacesPage() {
    });
    setPhotoLink(' ');
     }
-    function uploadPhoto(ev)
+     function uploadPhoto(ev)
     {
       const files=ev.target.files;
-      c
+      const data=new FormData();
+      for(i=0;i< files.length;i++)
+      {
+        data.append('photos' , files[i]);
+      }
+      
+      axios.post('/upload' , data, {
+        headers:{'Content-type': 'multipart/form-data'}
+      }).then(response => {
+        const {data:filename}=response;
+        setAddedPhotos(prev => {
+            return [...prev, filename];
+        })
+
+    })
     }
     return (
         <div>
@@ -86,7 +100,7 @@ export default function PlacesPage() {
                          ))}
 
                         <label className=" cursor-pointer flex items-center gap-1 jutify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
-                        <input type="file" className="hidden" onChange={uploadPhotoLink} />
+                        <input type="file" multiple className="hidden" onChange={uploadPhotoLink} />
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
                         </svg>
